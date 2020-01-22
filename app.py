a27655fbc4a4639afba5662fdbafd73af800c66e
CameronPennington <cameron.pennington@gmail.com>
2020-01-22 13:55:21 -0500

    artist search route working

--- app.py
+++ app.py
@@ -256,14 +256,14 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
+  artists = Artist.query.filter(func.lower(Artist.name).contains(
+    request.form.get('search_term').lower())).all()
+  
   response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+    "count": len(artists),
+    "data": artists
   }
+
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
