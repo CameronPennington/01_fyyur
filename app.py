f7cc2024bb1d53b8345bc12ebc4338d0d4cb0551
CameronPennington <cameron.pennington@gmail.com>
2020-03-06 06:50:20 -0500

    Commit before merging

--- app.py
+++ app.py
@@ -36,16 +36,16 @@ class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable = False)
+    name = db.Column(db.String(), nullable = False, unique=True)
     city = db.Column(db.String(120), nullable = False)
     state = db.Column(db.String(120), nullable = False)
     address = db.Column(db.String(120), nullable = False)
-    phone = db.Column(db.String(120), nullable = False)
-    website = db.Column(db.String(120), nullable = True)
+    phone = db.Column(db.String(120), nullable = False, unique=True)
+    website = db.Column(db.String(120), nullable = True, unique=True)
     seeking_talent = db.Column(db.Boolean, default = False)
     seeking_description = db.Column(db.String(500))
     image_link = db.Column(db.String(500), nullable = True)
-    facebook_link = db.Column(db.String(120))
+    facebook_link = db.Column(db.String(120), unique=True)
     shows = db.relationship('Show', backref='venue', lazy = True)
     genres = db.Column(db.ARRAY(db.String()))
 
@@ -75,13 +75,13 @@ class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable = False)
+    name = db.Column(db.String(), nullable = False, unique=True)
     genres = db.Column(db.ARRAY(db.String()))
     city = db.Column(db.String(120), nullable = False)
     state = db.Column(db.String(120), nullable = False)
-    phone = db.Column(db.String(120), nullable = False)
-    website = db.Column(db.String(120), nullable = True)
-    facebook_link = db.Column(db.String(120))
+    phone = db.Column(db.String(120), nullable = False, unique=True)
+    website = db.Column(db.String(120), nullable = True, unique=True)
+    facebook_link = db.Column(db.String(120), unique=True)
     seeking_venue = db.Column(db.Boolean, default = False)
     seeking_description = db.Column(db.String(500))
     image_link = db.Column(db.String(500), nullable = True)
